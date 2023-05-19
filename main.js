8f90e98547b0fafa3958b910f6768a3b089d4291
SGCBE <110899946+SGCBE@users.noreply.github.com>
2023-05-19 16:05:09 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // Configuration de Firebase
 const firebaseConfig = {
-  // Votre configuration Firebase
+  apiKey: "AIzaSyCRxjJPOHEBAbnXQariFN6funIWPpsIe28",
+  authDomain: "atelier---gestion-de-stock.firebaseapp.com",
+  databaseURL: "https://atelier---gestion-de-stock-default-rtdb.firebaseio.com",
+  projectId: "atelier---gestion-de-stock",
+  storageBucket: "atelier---gestion-de-stock.appspot.com",
+  messagingSenderId: "92935528444",
+  appId: "1:92935528444:web:57786855ed9cc7ef129c79"
 };
 
 // Initialisation de Firebase
