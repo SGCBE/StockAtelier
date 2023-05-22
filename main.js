1be7f6bf4c84ddca45172d53e13fccb645870127
SGCBE <110899946+SGCBE@users.noreply.github.com>
2023-05-22 16:06:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,137 +5,105 @@ const firebaseConfig = {
   databaseURL: "https://atelier---gestion-de-stock-default-rtdb.firebaseio.com",
   projectId: "atelier---gestion-de-stock",
   storageBucket: "atelier---gestion-de-stock.appspot.com",
-  messagingSenderId: "92935528444",
-  appId: "1:92935528444:web:57786855ed9cc7ef129c79"
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdefghijklmno"
 };
 
 // Initialisation de Firebase
 firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+const database = firebase.database();
 
-// Référence à la collection "equipments" dans la base de données
-const equipmentRef = db.ref("equipments");
+// Référence à la table "equipments"
+const equipmentsRef = database.ref('equipments');
 
 // Fonction pour ajouter un nouvel équipement
-function addEquipment(equipment) {
-  equipmentRef.push(equipment);
+function addEquipment(nom, description, quantite) {
+  const equipment = {
+    nom,
+    description,
+    quantite
+  };
+
+  equipmentsRef.push(equipment);
+}
+
+// Fonction pour générer la liste des équipements
+function generateEquipmentList(data) {
+  const equipmentTableBody = document.getElementById('equipmentTableBody');
+  equipmentTableBody.innerHTML = '';
+
+  data.forEach((equipment) => {
+    const equipmentRow = document.createElement('tr');
+    equipmentRow.innerHTML = `
+      <td>${equipment.nom}</td>
+      <td>${equipment.description}</td>
+      <td>${equipment.quantite}</td>
+      <td>
+        <button onclick="removeEquipment('${equipment.id}')">Supprimer</button>
+      </td>
+    `;
+    equipmentTableBody.appendChild(equipmentRow);
+  });
+
+  // Appel à la fonction de redimensionnement des colonnes après avoir généré la liste
+  resizeTableColumns();
 }
 
 // Fonction pour supprimer un équipement
-function deleteEquipment(equipmentId) {
-  const equipmentToDeleteRef = db.ref(`equipments/${equipmentId}`);
-  equipmentToDeleteRef.remove();
+function removeEquipment(equipmentId) {
+  const equipmentRef = database.ref(`equipments/${equipmentId}`);
+  equipmentRef.remove();
 }
 
-// Fonction de rappel pour mettre à jour la liste des équipements
-function updateEquipmentList(snapshot) {
-  const equipmentTableBody = document.getElementById("equipmentTableBody");
-  equipmentTableBody.innerHTML = "";
-
-  snapshot.forEach((childSnapshot) => {
-    const equipment = childSnapshot.val();
-    const equipmentId = childSnapshot.key;
-
-    const row = document.createElement("tr");
-    row.innerHTML = `
-      <td>${equipment.nom}</td>
-      <td>${equipment.description}</td>
-      <td>${equipment.quantite}</td>
-      <td>
-        <button onclick="selectEquipmentRow('${equipmentId}')">Voir les détails</button>
-        <button onclick="deleteEquipment('${equipmentId}')">Supprimer</button>
-      </td>
-    `;
-
-    equipmentTableBody.appendChild(row);
-  });
+// Fonction pour afficher le modal
+function showModal() {
+  const modal = document.getElementById('addEquipmentModal');
+  modal.style.display = 'block';
 }
-
-// Écouter les modifications dans la base de données
-equipmentRef.on("value", updateEquipmentList);
 
 // Fonction pour masquer le modal
 function hideModal() {
-  const addEquipmentModal = document.getElementById("addEquipmentModal");
-  addEquipmentModal.style.display = "none";
+  const modal = document.getElementById('addEquipmentModal');
+  modal.style.display = 'none';
 }
 
-// Gérer la soumission du formulaire d'ajout d'équipement
-const addEquipmentForm = document.getElementById("addEquipmentForm");
-addEquipmentForm.addEventListener("submit", (e) => {
+// Fonction pour redimensionner les colonnes de la table en fonction du contenu
+function resizeTableColumns() {
+  const autoResizableCells = document.querySelectorAll('.auto-resizable');
+
+  autoResizableCells.forEach(cell => {
+    cell.style.width = '1px'; // Réinitialiser la largeur à une valeur minimale pour calculer la taille réelle du contenu
+    const computedWidth = `${cell.scrollWidth}px`;
+    cell.style.width = computedWidth; // Appliquer la largeur calculée pour ajuster la colonne
+  });
+}
+
+// Appeler la fonction de redimensionnement au chargement de la page et lors des modifications de la fenêtre
+window.addEventListener('load', resizeTableColumns);
+window.addEventListener('resize', resizeTableColumns);
+
+// Écouteur d'événement pour le formulaire d'ajout d'équipement
+document.getElementById('addEquipmentForm').addEventListener('submit', (e) => {
   e.preventDefault();
+  const nom = document.getElementById('nom').value;
+  const description = document.getElementById('description').value;
+  const quantite = document.getElementById('quantite').value;
 
-  const nomInput = document.getElementById("nom");
-  const descriptionInput = document.getElementById("description");
-  const quantiteInput = document.getElementById("quantite");
-
-  const equipment = {
-    nom: nomInput.value,
-    description: descriptionInput.value,
-    quantite: quantiteInput.value
-  };
-
-  addEquipment(equipment);
-
-  nomInput.value = "";
-  descriptionInput.value = "";
-  quantiteInput.value = "";
-
+  addEquipment(nom, description, quantite);
   hideModal();
 });
 
-// Fonction pour afficher les détails de l'équipement dans le modal
-function showEquipmentDetails(equipment) {
-  const modalContent = document.querySelector(".modal-content");
+// Écouteur d'événement pour le bouton "Ajouter un équipement"
+document.getElementById('addEquipmentButton').addEventListener('click', showModal);
 
-  // Construire le contenu du modal avec les données de l'équipement
-  modalContent.innerHTML = `
-    <span class="close" onclick="hideModal()">&times;</span>
-    <h2>Détails de l'équipement</h2>
-    <p><strong>Nom:</strong> ${equipment.nom}</p>
-    <p><strong>Description:</strong> ${equipment.description}</p>
-    <p><strong>Quantité:</strong> ${equipment.quantite}</p>
-    <button onclick="hideModal()">Retour à la liste</button>
-  `;
-
-  // Afficher le modal
-  const addEquipmentModal = document.getElementById("addEquipmentModal");
-  addEquipmentModal.style.display = "block";
-}
-
-// Fonction pour afficher les détails de l'équipement lors de la sélection d'une ligne
-function selectEquipmentRow(equipmentId) {
-  // Récupérer les données de l'équipement sélectionné à partir de la base de données
-  const selectedEquipmentRef = db.ref(`equipments/${equipmentId}`);
-  selectedEquipmentRef.once("value", (snapshot) => {
-    const equipment = snapshot.val();
-    showEquipmentDetails(equipment);
-  });
-}
-
-// Fonction pour générer les lignes du tableau
-function generateTableRows(snapshot) {
-  const equipmentTableBody = document.getElementById("equipmentTableBody");
-  equipmentTableBody.innerHTML = "";
-
+// Événement pour détecter les modifications dans la base de données Firebase
+equipmentsRef.on('value', (snapshot) => {
+  const data = [];
   snapshot.forEach((childSnapshot) => {
-    const equipment = childSnapshot.val();
-    const equipmentId = childSnapshot.key;
-
-    const row = document.createElement("tr");
-    row.innerHTML = `
-      <td>${equipment.nom}</td>
-      <td>${equipment.description}</td>
-      <td>${equipment.quantite}</td>
-      <td>
-        <button onclick="selectEquipmentRow('${equipmentId}')">Voir les détails</button>
-        <button onclick="deleteEquipment('${equipmentId}')">Supprimer</button>
-      </td>
-    `;
-
-    equipmentTableBody.appendChild(row);
+    const childData = childSnapshot.val();
+    childData.id = childSnapshot.key;
+    data.push(childData);
   });
-}
 
-// Mettre à jour la liste des équipements lors de la modification de la base de données
-equipmentRef.on("value", generateTableRows);
+  generateEquipmentList(data);
+});
