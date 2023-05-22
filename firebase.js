f40d24bae8316a093c09f5dd99a7c8ccea125979
SGCBE <110899946+SGCBE@users.noreply.github.com>
2023-05-22 13:06:19 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRxjJPOHEBAbnXQariFN6funIWPpsIe28",
+  authDomain: "atelier---gestion-de-stock.firebaseapp.com",
+  databaseURL: "https://atelier---gestion-de-stock-default-rtdb.firebaseio.com",
+  projectId: "atelier---gestion-de-stock",
+  storageBucket: "atelier---gestion-de-stock.appspot.com",
+  messagingSenderId: "92935528444",
+  appId: "1:92935528444:web:57786855ed9cc7ef129c79"
+};
