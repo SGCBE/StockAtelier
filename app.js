dbb3b9d9ad66119154805eda461ae179459a3386
SGCBE <110899946+SGCBE@users.noreply.github.com>
2023-05-30 17:13:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -96,22 +96,55 @@ categoryFilter.addEventListener('change', (event) => {
   }
 });
 
-// Fonction pour ajouter un équipement
-function addEquipment() {
+// Fonction pour ouvrir le modal d'ajout d'équipement
+function openAddEquipmentModal() {
+  const modal = document.getElementById('add-equipment-modal');
+  modal.style.display = 'block';
+}
+
+// Fonction pour fermer le modal d'ajout d'équipement
+function closeAddEquipmentModal() {
+  const modal = document.getElementById('add-equipment-modal');
+  modal.style.display = 'none';
+}
+
+// Écouteur d'événement pour le clic sur le bouton "Ajouter un équipement"
+const addEquipmentBtn = document.getElementById('add-equipment-btn');
+addEquipmentBtn.addEventListener('click', () => openAddEquipmentModal());
+
+// Écouteur d'événement pour le clic sur le bouton "Annuler" dans le modal d'ajout d'équipement
+const cancelAddEquipmentBtn = document.getElementById('cancel-add-equipment-btn');
+cancelAddEquipmentBtn.addEventListener('click', () => closeAddEquipmentModal());
+
+// Écouteur d'événement pour le clic sur le bouton "Enregistrer" dans le modal d'ajout d'équipement
+const saveEquipmentBtn = document.getElementById('save-equipment-btn');
+saveEquipmentBtn.addEventListener('click', () => {
+  // Récupérer les valeurs des champs de saisie
+  const categorie = document.getElementById('categorie-input').value;
+  const designation = document.getElementById('designation-input').value;
+  const quantite = document.getElementById('quantite-input').value;
+  const marque = document.getElementById('marque-input').value;
+  const modele = document.getElementById('modele-input').value;
+  const dimensions = document.getElementById('dimensions-input').value;
+  const prixHT = document.getElementById('prixHT-input').value;
+
+  // Créer un nouvel équipement avec les valeurs saisies
   const newEquipment = {
-    categorie: "Nouvelle catégorie",
-    designation: "Nouvelle désignation",
-    quantite: 0,
-    marque: "Nouvelle marque",
-    modele: "Nouveau modèle",
-    dimensions: "Nouvelles dimensions",
-    prixHT: 0
+    categorie: categorie,
+    designation: designation,
+    quantite: quantite,
+    marque: marque,
+    modele: modele,
+    dimensions: dimensions,
+    prixHT: prixHT
   };
 
+  // Ajouter le nouvel équipement à la base de données Firestore
   db.collection("equipments")
     .add(newEquipment)
     .then((docRef) => {
       console.log("Nouvel équipement ajouté avec ID :", docRef.id);
+      closeAddEquipmentModal();
       // Actualiser la liste des équipements
       db.collection('equipments')
         .get()
@@ -130,7 +163,7 @@ function addEquipment() {
     .catch((error) => {
       console.error("Erreur lors de l'ajout de l'équipement :", error);
     });
-}
+});
 
 // Charger tous les équipements lors du chargement initial de la page
 db.collection('equipments')
